631c56990981d19b9df129b73e3d78b01ce2acfb
Prakash Singh <prakashsingh98310@gmail.com>
2024-09-01 00:01:09 +0530

    footer new service section added

--- footer-design/script_footer.js
+++ footer-design/script_footer.js
@@ -2,16 +2,19 @@
 var contactModal = document.getElementById("contactModal");
 var portfolioModal = document.getElementById("portfolioModal");
 var teamModal = document.getElementById("teamModal");
+var serviceModal = document.getElementById("serviceModal");
 
 // Get the link elements
 var contactLink = document.getElementById("contactLink");
 var portfolioLink = document.getElementById("portfolioLink");
 var teamLink = document.getElementById("teamLink");
+var serviceLink = document.getElementById("serviceLink");
 
 // Get the close button elements
 var contactClose = document.getElementsByClassName("close")[0];
 var portfolioClose = document.getElementsByClassName("close2")[0];
 var teamClose = document.getElementsByClassName("close3")[0];
+var serviceClose = document.getElementsByClassName("close4")[0];
 
 // When the user clicks on the contact link, open the contact modal
 contactLink.onclick = function(event) {
@@ -31,6 +34,12 @@ teamLink.onclick = function(event) {
     teamModal.style.display = "block";
 }
 
+// When the user clicks on the service link, open the service modal
+serviceLink.onclick = function(event) {
+    event.preventDefault(); 
+    serviceModal.style.display = "block";
+}
+
 // Close modals when the close button is clicked
 contactClose.onclick = function() {
     contactModal.style.display = "none";
@@ -44,6 +53,10 @@ teamClose.onclick = function() {
     teamModal.style.display = "none";
 }
 
+serviceClose.onclick = function() {
+    serviceModal.style.display = "none";
+}
+
 // Close modals when the user clicks outside of them
 window.onclick = function(event) {
     if (event.target == contactModal) {
@@ -54,5 +67,8 @@ window.onclick = function(event) {
     }
     if (event.target == teamModal) {
         teamModal.style.display = "none";
+    }
+    if (event.target == serviceModal) {
+        serviceModal.style.display = "none";
     }
 }
